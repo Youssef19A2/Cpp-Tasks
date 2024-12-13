e715d4c011fa046f0ac29d360876c8e27d1f6a71
Youssef19A2 <youssefahmed36567@gmail.com>
2024-12-13 20:00:38 +0200

    Add files via upload

---
+++ Task_2_6.cpp
@@ -0,0 +1,14 @@
+//  Write a lambda function to calculate the square of a given number.
+
+#include <iostream>
+
+int main() {
+    auto square = [](int x) { return x * x; };
+    int num;
+    std::cout << "Enter a number to find its square: ";
+    std::cin >> num;
+    int result = square(num);
+    std::cout << "The square of " << num << " is " << result << std::endl;
+
+    return 0;
+}
