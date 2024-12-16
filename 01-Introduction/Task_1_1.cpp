109f674d0db314306d34b6949b336892fef17c36
Youssef19A2 <youssefahmed36567@gmail.com>
2024-12-17 01:14:55 +0200

    Add files via upload

---
+++ 01-Introduction/Task_1_1.cpp
@@ -0,0 +1,11 @@
+//1-Create a table for AscII code
+#include <iostream>
+
+int main() {
+    char c ;
+    std::cout<<"Enter The char: "<<std::endl;
+    std::cin>>c;
+    int ascii_value = int(c); 
+    std::cout << "The ASCII value of " << c << " is " << ascii_value << "\n";
+    return 0;
+}
